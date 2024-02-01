b79a6c91066d21ef3e0c638e734296fa156b247c
ZivLow <zivlowenhao@gmail.com>
2024-02-01 17:11:26 +0800

    Add copyright notice and initialize current sensor event callback

--- main/DeviceCallbacks.cpp
+++ main/DeviceCallbacks.cpp
@@ -1,3 +1,19 @@
+/*
+ *    Copyright 2024 Ziv Low
+ *    
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *    
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *    
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 #include "AppTask.h"
 #include "DeviceCallbacks.h"
 #include "LEDWidget.h"
@@ -65,5 +81,17 @@ exit:
 void emberAfOnOffClusterInitCallback(EndpointId endpoint)
 {
     ESP_LOGI(TAG, "emberAfOnOffClusterInitCallback");
-    GetAppTask().UpdateClusterState();
+    GetAppTask().LightEventHandler();
+}
+
+void emberAfElectricalMeasurementClusterInitCallback(EndpointId endpoint)
+{
+    ESP_LOGI(TAG, "emberAfElectricalMeasurementClusterInitCallback");
+
+    CurrentSensorEvent currentSensorEvent;
+    currentSensorEvent.DcVoltage = 0;
+    currentSensorEvent.DcCurrent = 0;
+    currentSensorEvent.DcPower = 0;
+
+    GetAppTask().CurrentSensorEventHandler(currentSensorEvent);
 }
