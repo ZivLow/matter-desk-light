1b62c19030a47bdc6f09b8ac50ea216e504c20d4
ZivLow <zivlowenhao@gmail.com>
2024-02-01 17:15:17 +0800

    LEDWidget brightness initializes to 0

--- main/LEDWidget.cpp
+++ main/LEDWidget.cpp
@@ -1,3 +1,19 @@
+/*
+ *    Copyright 2024 Ziv Low
+ *    
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *    
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *    
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 #include "LEDWidget.h"
 #include "esp_log.h"
 
@@ -8,7 +24,7 @@ void LEDWidget::Init(void)
     esp_log_level_set(TAG, ESP_LOG_DEBUG);
 
     mState      = false;
-    mBrightness = UINT8_MAX/2;
+    mBrightness = 0;    // UINT8_MAX/2;
     mGPIONum    = (gpio_num_t) CONFIG_LED_GPIO;
 
     ledc_timer_config_t ledc_timer = {
