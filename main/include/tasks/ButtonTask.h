00d6d36c8b5029042be9842b4ee741b0c1e04f16
ZivLow <zivlowenhao@gmail.com>
2024-02-01 17:21:26 +0800

    Refactor ButtonTask class and add Button struct

--- main/include/tasks/ButtonTask.h
+++ main/include/tasks/ButtonTask.h
@@ -1,45 +1,72 @@
+/*
+ *    Copyright 2024 Ziv Low
+ *    
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *    
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *    
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 #pragma once
 
 #include "AppEvent.h"
-#include "AppTask.h"
 #include "LEDWidget.h"
 #include <platform/CHIPDeviceLayer.h>
 #include "driver/touch_pad.h"
-//#include "soc/rtc_periph.h"
 #include "soc/sens_periph.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/queue.h"
+#include <vector>
+#include <bitset>
+#include <memory>
 
 // Application-defined error codes in the CHIP_ERROR space.
 #define APP_ERROR_EVENT_QUEUE_FAILED CHIP_APPLICATION_ERROR(0x01)
 #define APP_ERROR_CREATE_TASK_FAILED CHIP_APPLICATION_ERROR(0x02)
 #define APP_ERROR_UNHANDLED_EVENT CHIP_APPLICATION_ERROR(0x03)
 
-#define TOUCHPAD_FILTER_TOUCH_PERIOD 10     // Period in ms
+#define TOUCHPAD_FILTER_TOUCH_PERIOD 20     // Period in ms
 
 extern LEDWidget AppLED;
 
 class ButtonTask
 {
+public:
+    struct Buttons {
+        std::bitset<16> ButtonState;
+        std::vector<touch_pad_t> EnabledButtons;
+
+        explicit Buttons(const std::vector<touch_pad_t> &buttons) :
+            EnabledButtons(buttons)
+            {}
+    };
+
+    
 
 public:
     CHIP_ERROR StartButtonTask();
-    CHIP_ERROR Init(AppEvent *aEvent);
+    CHIP_ERROR Init(const std::unique_ptr<Buttons> &pButtons);
     static void ButtonTaskMain(void * pvParameter);
-    static void ButtonActionEventHandler(AppEvent *aEvent);
+    static void ButtonTaskReadRaw(void * pvParameter);
     static void button_rtc_intr(void *arg);
 
 private:
     friend ButtonTask & GetButtonTask(void);
-    static void button_set_thresholds(AppEvent *aEvent);
-    static void ButtonBothPressedAction(AppEvent *aEvent, LEDWidget *led);
-    static void ButtonBrightnessIncreaseAction(AppEvent *aEvent, LEDWidget *led, const uint8_t &change);
-    static void ButtonBrightnessDecreaseAction(AppEvent *aEvent, LEDWidget *led, const uint8_t &change);
-    
-    static ButtonTask sButtonTask;
+    static void button_set_thresholds(const std::unique_ptr<Buttons> &pButtons);
+    static void ButtonBothPressedAction(LEDWidget *led);
+    static void ButtonBrightnessIncreaseAction(LEDWidget *led, const uint8_t &change);
+    static void ButtonBrightnessDecreaseAction(LEDWidget *led, const uint8_t &change);
 };
 
 inline ButtonTask & GetButtonTask(void)
 {
-    return ButtonTask::sButtonTask;
+    static ButtonTask sButtonTask;
+    return sButtonTask;
 }
--- main/tasks/ButtonTask.cpp
+++ main/tasks/ButtonTask.cpp
@@ -1,4 +1,21 @@
+/*
+ *    Copyright 2024 Ziv Low
+ *    
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *    
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *    
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 #include "ButtonTask.h"
+#include "AppTask.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/queue.h"
@@ -6,12 +23,11 @@
 #include "esp_log.h"
 #include "esp_err.h"
 #include <ranges>
+#include <memory>
 
 static const char *const TAG = "button-task";
 static TaskHandle_t sButtonTaskHandle;
 static EventGroupHandle_t sButtonEventGroup;
-
-ButtonTask ButtonTask::sButtonTask;
 
 CHIP_ERROR ButtonTask::StartButtonTask()
 {
@@ -30,7 +46,7 @@ CHIP_ERROR ButtonTask::StartButtonTask()
     return (xReturned == pdPASS) ? CHIP_NO_ERROR : APP_ERROR_CREATE_TASK_FAILED;
 }
 
-CHIP_ERROR ButtonTask::Init(AppEvent *aEvent)
+CHIP_ERROR ButtonTask::Init(const std::unique_ptr<ButtonTask::Buttons> &pButtons)
 {
     CHIP_ERROR err = CHIP_NO_ERROR;
 
@@ -45,8 +61,8 @@ CHIP_ERROR ButtonTask::Init(AppEvent *aEvent)
     touch_pad_set_voltage(TOUCH_HVOLT_2V7, TOUCH_LVOLT_0V5, TOUCH_HVOLT_ATTEN_1V);
 
     // t_sleep = sleep_cycle / SOC_CLK_RC_SLOW_FREQ_APPROX. SOC_CLK_RC_SLOW_FREQ_APPROX=150000. sleep_cycle [0, 0xFFFF]. Max 0.4369sec
-    // t_meas = meas_cycle / SOC_CLK_RC_FAST_FREQ_APPROX. SOC_CLK_RC_FAST_FREQ_APPROX=8500000. meas_cycle [0, 0xFFFF]
-    touch_pad_set_meas_time(0.25*SOC_CLK_RC_SLOW_FREQ_APPROX, 20000);
+    // t_meas = meas_cycle / SOC_CLK_RC_FAST_FREQ_APPROX. SOC_CLK_RC_FAST_FREQ_APPROX=8500000. meas_cycle [0, 0xFFFF]. Max 0.00771sec
+    touch_pad_set_meas_time(0.05*SOC_CLK_RC_SLOW_FREQ_APPROX, 30000);
 
     uint16_t sleep_cycle, meas_cycle;
 
@@ -54,15 +70,14 @@ CHIP_ERROR ButtonTask::Init(AppEvent *aEvent)
     ESP_LOGD(TAG, "sleep_cycle is %" PRIu16 ", meas_cycle is %" PRIu16, sleep_cycle, meas_cycle);
 
     // Init touch pad IO
-    for (const auto& [idx, button_enabled] : aEvent->pButtonEvent->ButtonEnabled | std::views::enumerate | std::views::as_const)
-        if (button_enabled) touch_pad_config(static_cast<touch_pad_t>(idx), 0);
+    for (const auto& buttons : pButtons->EnabledButtons)
+        touch_pad_config(buttons, 0);
     // Initialize and start a software filter to detect slight change of capacitance.
     touch_pad_filter_start(TOUCHPAD_FILTER_TOUCH_PERIOD);
     // Set thresh hold
-    button_set_thresholds(aEvent);
-    aEvent->mHandler = GetAppTask().LightingActionEventHandler;
+    button_set_thresholds(pButtons);
     // Register touch interrupt ISR
-    esp_err_t esp_err = touch_pad_isr_register(button_rtc_intr, aEvent);
+    esp_err_t esp_err = touch_pad_isr_register(button_rtc_intr, NULL);
     if (esp_err != ESP_OK)
     {
         ESP_LOGE(TAG, "touch pad isr register error %d", esp_err);
@@ -74,34 +89,18 @@ CHIP_ERROR ButtonTask::Init(AppEvent *aEvent)
     return err;
 }
 
-/*
-  Read values sensed at all available touch pads.
-  Use 2 / 3 of read value as the threshold
-  to trigger interrupt when the pad is touched.
-  Note: this routine demonstrates a simple way
-  to configure activation threshold for the touch pads.
-  Do not touch any pads when this routine
-  is running (on application start).
- */
-void ButtonTask::button_set_thresholds(AppEvent *aEvent)
+void ButtonTask::button_set_thresholds(const std::unique_ptr<ButtonTask::Buttons> &pButtons)
 {
     uint16_t touch_value;
 
-    for (const auto& [idx, button_enabled] : aEvent->pButtonEvent->ButtonEnabled | std::views::enumerate | std::views::as_const)
+    for (const auto& buttons : pButtons->EnabledButtons)
     {
-        if (button_enabled)
-        {
-            touch_pad_read_filtered(static_cast<touch_pad_t>(idx), &touch_value);
-            ESP_LOGD(TAG, "button_set_threshold: touch pad [%d] val is %d", idx, touch_value);
-            ESP_ERROR_CHECK(touch_pad_set_thresh(static_cast<touch_pad_t>(idx), touch_value * 2 / 3));
-        }
+        touch_pad_read_filtered(buttons, &touch_value);
+        ESP_LOGD(TAG, "button_set_threshold: touch pad [%d] val is %d", buttons, touch_value);
+        ESP_ERROR_CHECK(touch_pad_set_thresh(buttons, touch_value * 0.4));
     }
 }
 
-/*
-  Handle an interrupt triggered when a pad is touched.
-  Recognize what pad has been touched and save it in a table.
- */
 void ButtonTask::button_rtc_intr(void *arg)
 {
     uint32_t pad_intr = touch_pad_get_status();
@@ -146,18 +145,13 @@ void ButtonTask::button_rtc_intr(void *arg)
 
 void ButtonTask::ButtonTaskMain(void * pvParameter)
 {
-    AppEvent *aEvent = new AppEvent();
-    aEvent->pButtonEvent = new ButtonEvent();
+    static std::unique_ptr<Buttons> pButtons;
+    pButtons = std::make_unique<Buttons>(std::vector<touch_pad_t>{TOUCH_PAD_NUM8, TOUCH_PAD_NUM9});
 
-    // Config enable Button8 & Button9
-    aEvent->pButtonEvent->ButtonEnabled[8] = true;
-    aEvent->pButtonEvent->ButtonEnabled[9] = true;
-
-    CHIP_ERROR err = sButtonTask.Init(aEvent);
+    CHIP_ERROR err = GetButtonTask().Init(pButtons);
     if (err != CHIP_NO_ERROR)
     {
         ESP_LOGI(TAG, "ButtonTask.Init() failed due to %" CHIP_ERROR_FORMAT, err.Format());
-        delete aEvent;
         return;
     }
 
@@ -166,9 +160,9 @@ void ButtonTask::ButtonTaskMain(void * pvParameter)
     // Change brightness value by 30 per switch
     const uint8_t change = 30;
     EventBits_t uxBits;
+    ButtonEvent buttonEvent;
 
     while (true) {
-
         uxBits = xEventGroupWaitBits(
                     sButtonEventGroup,   /* The event group being tested. */
                     (1 << 8) | (1 << 9), /* The bits within the event group to wait for. */
@@ -176,23 +170,31 @@ void ButtonTask::ButtonTaskMain(void * pvParameter)
                     pdFALSE,       /* pdFALSE=Don't wait for both bits, either bit will do. */
                     portMAX_DELAY );/* How to long to wait for either bit to be set. */
 
-        aEvent->pButtonEvent->ButtonStateRaw = uxBits;
+        pButtons->ButtonState = uxBits;
 
-        if (aEvent->pButtonEvent->ButtonState.Button8 && aEvent->pButtonEvent->ButtonState.Button9){
-            ButtonBothPressedAction(aEvent, &AppLED);
-        }
-        else if (aEvent->pButtonEvent->ButtonState.Button9){
-            ButtonBrightnessIncreaseAction(aEvent, &AppLED, change);
-        }
-        else if (aEvent->pButtonEvent->ButtonState.Button8){
-            ButtonBrightnessDecreaseAction(aEvent, &AppLED, change);
+        if (pButtons->ButtonState.none()) {
+            ESP_LOGD(TAG, "None of the bits of the buttons are set");
+            continue;
         }
 
-        aEvent->pButtonEvent->ButtonStateRaw = 0;
+        if (pButtons->ButtonState.test(TOUCH_PAD_NUM8) && pButtons->ButtonState.test(TOUCH_PAD_NUM9)){
+            ButtonBothPressedAction(&AppLED);
+        }
+        else if (pButtons->ButtonState.test(TOUCH_PAD_NUM9)){
+            ButtonBrightnessIncreaseAction(&AppLED, change);
+        }
+        else if (pButtons->ButtonState.test(TOUCH_PAD_NUM8)){
+            ButtonBrightnessDecreaseAction(&AppLED, change);
+        }
+
+        buttonEvent.ButtonStateRaw = uxBits;
+        GetAppTask().ButtonEventHandler(buttonEvent);
+
+        pButtons->ButtonState = 0;
     }
 }
 
-void ButtonTask::ButtonBothPressedAction(AppEvent *aEvent, LEDWidget *led)
+void ButtonTask::ButtonBothPressedAction(LEDWidget *led)
 {
     ESP_LOGI(TAG, "Both Buttons Pressed!");
 
@@ -201,11 +203,9 @@ void ButtonTask::ButtonBothPressedAction(AppEvent *aEvent, LEDWidget *led)
     }
 
     led->Toggle();
-
-    GetAppTask().PostEvent(aEvent);
 }
 
-void ButtonTask::ButtonBrightnessIncreaseAction(AppEvent *aEvent, LEDWidget *led, const uint8_t &change)
+void ButtonTask::ButtonBrightnessIncreaseAction(LEDWidget *led, const uint8_t &change)
 {
     ESP_LOGI(TAG, "Button Brightness Increase Pressed!");
 
@@ -224,11 +224,9 @@ void ButtonTask::ButtonBrightnessIncreaseAction(AppEvent *aEvent, LEDWidget *led
     {
         led->Toggle();
     }
-
-    GetAppTask().PostEvent(aEvent);
 }
 
-void ButtonTask::ButtonBrightnessDecreaseAction(AppEvent *aEvent, LEDWidget *led, const uint8_t &change)
+void ButtonTask::ButtonBrightnessDecreaseAction(LEDWidget *led, const uint8_t &change)
 {
     ESP_LOGI(TAG, "Button Brightness Decrease Pressed!");
 
@@ -250,6 +248,4 @@ void ButtonTask::ButtonBrightnessDecreaseAction(AppEvent *aEvent, LEDWidget *led
     {
         led->Toggle();
     }
-
-    GetAppTask().PostEvent(aEvent);
 }
