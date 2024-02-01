0d73c71d108665386b8fa2ae84e9a17e1791fd67
ZivLow <zivlowenhao@gmail.com>
2024-02-01 17:16:04 +0800

    Refactor AppEvent struct and add CurrentSensorEvent

--- main/include/tasks/AppEvent.h
+++ main/include/tasks/AppEvent.h
@@ -1,14 +1,31 @@
+/*
+ *    Copyright 2024 Ziv Low
+ *    
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *    
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *    
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 #pragma once
 
 #include <stdint.h>
 #include "hal/touch_sensor_types.h"
-#include <array>
 
 typedef struct Buttons Buttons;
 typedef struct ButtonEvent ButtonEvent;
 typedef struct LightEvent LightEvent;
+typedef struct CurrentSensorEvent CurrentSensorEvent;
 typedef struct AppEvent AppEvent;
-typedef void (*EventHandler)(AppEvent *);
+using EventHandler = void (*)(const AppEvent &);
+
 
 struct Buttons {
     bool Button0  : 1;
@@ -35,20 +52,25 @@ struct ButtonEvent {
         Buttons ButtonState;
         uint32_t ButtonStateRaw : 16;
     };
-
-    std::array<bool, TOUCH_PAD_MAX> ButtonEnabled{};
 };
 
 struct LightEvent {
-    uint8_t Action;
-    int32_t Actor;
+    bool LightState;
+    uint8_t LightLevel;
+};
+
+struct CurrentSensorEvent {
+    int16_t DcVoltage;
+    int16_t DcCurrent;
+    int16_t DcPower;
 };
 
 struct AppEvent {
     union
     {
-        ButtonEvent *pButtonEvent;
-        LightEvent *pLightEvent;
+        ButtonEvent mButtonEvent;
+        LightEvent mLightEvent;
+        CurrentSensorEvent mCurrentSensorEvent;
     };
 
     EventHandler mHandler;
